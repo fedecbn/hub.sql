a6369df9f9fe48f5617cdd1c6ce18b8d3004e7ba
TomMilon <tom_milon@hotmail.com>
2016-09-08 11:35:07 +0200

    hub_siflore_publicate.sh
    ajout de la mise à jour des synthese et ref

--- sh/hub_siflore_publicate.sh
+++ sh/hub_siflore_publicate.sh
@@ -13,6 +13,9 @@ while read  lib_schema pipe1 jdd pipe2 version; do
  psql -q -p 5432 -c "SELECT * FROM siflore_data_refresh('$lib_schema','$jdd',$version);"  si_flore_national_v4
 done
 
+#mise à jour des synthèses et réf (liste taxon)
+psql -q -p 5432 -c "SELECT * FROM siflore_synthese_refresh();"  si_flore_national_v4
+
 # on vide la table queue
 psql -q -p 5433 -c "TRUNCATE public.publicating_queue;" si_flore_national
 
